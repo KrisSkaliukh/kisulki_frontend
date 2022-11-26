d988c62d6df82e4a5ef96e0ba51e087e9d2e250d
Kris Skaliukh <k.skaliukh@tg.dunice.net>
2022-11-26 14:24:36 +0300

    old student page with animate

--- src/pages/StudentPage/StudentPage.tsx
+++ src/pages/StudentPage/StudentPage.tsx
@@ -1,15 +1,50 @@
-import { memo } from 'react';
+import cn from 'classnames';
+import { memo, useState } from 'react';
 
 import styles from './student-page.module.scss';
 
 function StudentPage() {
+  const [isExpanded, setIsExpanded] = useState('none');
+
+  const selectMode = (mode: string) => {
+    setIsExpanded(mode);
+  };
+
   return (
     <div className={styles.content}>
-      <div className={styles.lectureOffline}>
-        <div className={styles.lectureText}>Вход на очную лекцию</div>
+      <div
+        role="none"
+        onClick={() => selectMode('offline')}
+        className={cn({
+          [styles.lectureOffline]: isExpanded === 'none',
+          [styles.expandedOffline]: isExpanded === 'offline',
+          [styles.hideOffline]: isExpanded === 'online',
+        })}
+      >
+        <div className={isExpanded !== 'online' ? styles.lectureTextOffline : styles.hiddenText}>
+          Регистрация
+          {' '}
+          <br />
+          {' '}
+          на очной лекции
+        </div>
       </div>
-      <div className={styles.lectureOnline}>
-        <div className={styles.lectureTextOnline}>Вход на дистанционную лекцию</div>
+      <div
+        role="none"
+        onClick={() => selectMode('online')}
+        className={cn({
+          [styles.lectureOnline]: isExpanded === 'none',
+          [styles.expandedOnline]: isExpanded === 'online',
+          [styles.hideOnline]: isExpanded === 'offline',
+        })}
+      >
+        <div className={isExpanded !== 'offline' ? styles.lectureTextOnline : styles.hiddenText}>
+          Регистрация
+          {' '}
+          <br />
+          {' '}
+          на дистанционной лекции
+        </div>
       </div>
     </div>
   );
